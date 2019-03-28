724510ec1de4696eb6dbd26e81d676d85a36ea4c
Ben Bariteau <ben.bariteau@gmail.com>
2019-03-28 10:30:35 -0700

    add router middleware

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 extern crate iron;
+extern crate router;
 
 use iron::{Request, Response, IronResult, Chain, Iron};
+use router::Router;
 
 fn hello_world(request: &mut Request) -> IronResult<Response> {
     Ok(Response::with((iron::status::Ok, "Hello World")))
 }
 
 fn main() {
+    let mut router = Router::new();
+    router.get("/", hello_world, "home");
     let mut chain = Chain::new(hello_world);
     Iron::new(chain).http("localhost:8080").unwrap();
 }
