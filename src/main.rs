2a1cd9aaa945f0b528d9679c730f1ce3374bbdc6
Ben Bariteau <ben.bariteau@gmail.com>
2019-03-28 10:24:36 -0700

    setup basic iron scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate iron;
+
+use iron::{Request, Response, IronResult, Chain, Iron};
+
+fn hello_world(request: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((iron::status::Ok, "Hello World")))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut chain = Chain::new(hello_world);
+    Iron::new(chain).http("localhost:8080").unwrap();
 }
