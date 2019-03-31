0cd128119f45680177b446261d3b093447631023
Ben Bariteau <ben.bariteau@gmail.com>
2019-03-31 14:05:04 -0700

    add schemas for user and user_private

--- src/schema.rs
+++ src/schema.rs
@@ -5,3 +5,18 @@ table! {
         last_visit_time -> VarChar,
     }
 }
+
+table! {
+    user {
+        id -> Integer,
+        name -> VarChar,
+    }
+}
+
+table! {
+    user_private {
+        id -> Integer,
+        user_id -> Integer,
+        password_hash -> VarChar,
+    }
+}
